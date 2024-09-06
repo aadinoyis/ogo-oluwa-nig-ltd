0fdcdae75f3046ba3e0e33bb54ff6da8b4fe75ba
aadinoyis <mail.aadinoyis@gmail.com>
2024-09-06 09:27:07 +0100

    update

---
+++ main.js
@@ -0,0 +1,395 @@
+
+import {insertDb, selectDb} from './supabase.js';
+
+const print_page = document.querySelector('#print_page')
+print_page.onclick = e => {
+  window.print()
+}
+
+const formatDigits = (digits) => {
+  const value = digits !== null ? digits.replace(/,/g, '') : '';
+  const formattedValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return formattedValue;
+}
+
+const fromPeriod = document.querySelector('#fromPeriod')
+const toPeriod = document.querySelector('#toPeriod')
+const fromPeriodDisp = document.querySelector('#fromPeriodDisp')
+const toPeriodDisp = document.querySelector('#toPeriodDisp')
+
+// const exit_fromPeriod = document.querySelector('#exit_fromPeriod')
+// const exit_toPeriod = document.querySelector('#exit_toPeriod')
+const _nextPeriod = document.querySelector('#_nextPeriod')
+const toggle_nextPeriod = document.querySelector('#toggle_nextPeriod')
+
+let is_nextPeriod = false
+
+toggle_nextPeriod.onclick = function (e) {
+  if (is_nextPeriod) {
+    toPeriod.value = null;
+    nextData = null
+    toPeriodDisp.innerHTML = 'DD/MM/YY';
+    _nextPeriod.style.display = 'none'
+    this.innerHTML = '&plus;'
+    logDisplay()
+    return is_nextPeriod = false
+  }
+  
+  toPeriod.value = null;
+  nextData = null
+  toPeriodDisp.innerHTML = 'DD/MM/YY';
+  _nextPeriod.style.display = 'block'
+  this.innerHTML = '&minus;'
+  logDisplay()
+  return is_nextPeriod = true
+}
+
+// exit_toPeriod.onclick = e => {
+//   toPeriod.value = null;
+//   toPeriodDisp.innerHTML = '--pick end date--';
+//   nextData = null
+// }
+
+let dateFormat = {
+  weekday: 'short', 
+  day: '2-digit', 
+  month: 'long', 
+  year: 'numeric'
+}
+
+
+fromPeriod.value = null;
+fromPeriodDisp.innerHTML = 'DD/MM/YY'
+
+toPeriod.value = null;
+toPeriodDisp.innerHTML = 'DD/MM/YY'
+
+
+let prevData = null
+let nextData = null
+
+// const formattedNumber = number.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+
+const displayData = (data) => {
+  console.log(data)
+  // date: document.querySelector(''),
+  document.querySelector('#price_yy').innerHTML = `&#8358; ${formatDigits(data.price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#closing_yy').innerHTML = formatDigits(data.closing.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))
+  document.querySelector('#opening_yy').innerHTML = formatDigits(data.opening.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))
+  document.querySelector('#litres_yy').innerHTML = formatDigits(data.litres.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))
+  document.querySelector('#amount_yy').innerHTML = `&#8358; ${formatDigits(data.amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#cash_at_hand_yy').innerHTML = `&#8358; ${formatDigits(data.cash_at_hand.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#cash_to_bank_yy').innerHTML = `&#8358; ${formatDigits(data.cash_to_bank.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#expenses_yy').innerHTML = `&#8358; ${formatDigits(data.expenses.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#pos_yy').innerHTML = `&#8358; ${formatDigits(data.pos.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#balance_yy').innerHTML = `&#8358; ${formatDigits(data.balance.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))}`
+  document.querySelector('#shortage_yy').innerHTML = `&#8358; ${formatDigits(data.shortage.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }))} &uarr;&darr;`
+}
+
+const logDisplay = () => {
+  if (prevData !== null || nextData !== null) {
+    if(prevData !== null && nextData === null) {
+      displayData(prevData)
+    }
+    if(prevData !== null && nextData !== null) {
+      let result = {
+        price: '--',
+        closing: '--',
+        opening: '--',
+        litres: 0.00,
+        amount: 0.00,
+        expenses: 0.00,
+        pos: 0.00,
+        balance: 0.00,
+        cash_at_hand: 0.00,
+        cash_to_bank: 0.00,
+        shortage: 0.00,
+      }
+      // nextData.forEach(item => {
+      //   result.balance += item.balance;
+      //   result.cash_at_hand += item.cash_at_hand;
+      //   result.cash_to_bank += item.cash_to_bank;
+      //   result.shortage += item.shortage;
+      // });
+      for (let item of nextData) {
+        result.litres += item.litres
+        result.amount += item.amount
+        result.expenses += item.expenses
+        result.pos += item.pos
+        result.balance += item.balance
+        result.cash_at_hand += item.cash_at_hand
+        result.cash_to_bank += item.cash_to_bank
+        result.shortage += item.shortage
+      }
+      
+      displayData(result)
+    }
+    
+    
+    // else {
+    //   let data = {
+    //     price: '--',
+    //     closing: '--',
+    //     opening: '--',
+    //     litres: '--',
+    //     amount: '--',
+    //     expenses: '--',
+    //     pos: '--',
+    //     balance: '--',
+    //     cash_at_hand: '--',
+    //     cash_to_bank: '--',
+    //     shortage: '--',
+    //   }
+      
+    //   displayData(data)
+    // }
+  } 
+  if (prevData === null && nextData === null) {
+  
+    let data = {
+      price: '0.00',
+      closing: '0.00',
+      opening: '0.00',
+      litres: '0.00',
+      amount: '0.00',
+      expenses: '0.00',
+      pos: '0.00',
+      balance: '0.00',
+      cash_at_hand: '0.00',
+      cash_to_bank: '0.00',
+      shortage: '0.00',
+    }
+    
+    displayData(data)
+  }
+}
+logDisplay()
+
+fromPeriod.addEventListener('change', async e => {
+  fromPeriodDisp.innerHTML = new Date(fromPeriod.value).toLocaleDateString('en-US', dateFormat)
+  const data = await selectDb(fromPeriod.value, null)
+  if (data) {
+    prevData = data
+    logDisplay()
+  }
+})
+
+toPeriod.addEventListener('change', async e => {
+  toPeriodDisp.innerHTML = new Date(toPeriod.value).toLocaleDateString('en-US', dateFormat)
+  const data = await selectDb(fromPeriod.value, toPeriod.value)
+  if (data) {
+    nextData = data
+    logDisplay()
+  }
+})
+
+
+
+const create = document.querySelector('#create')
+const createNew = document.querySelector('#createNew')
+const header = document.querySelector('header')
+const main = document.querySelector('main')
+
+let opened = false;
+create.addEventListener('click', e => {
+  if (opened) {
+    // slide down till it disapears
+    createNew.style.height = '0'
+    
+    // change thw create text to '+'
+    create.innerHTML = "&plus;"
+    
+    // bold background elements
+    header.style.opacity = '1'
+    main.style.opacity = '1'
+    
+    // set opened to false
+    return opened = false
+    
+  } else {
+    // slide up till 80% of screen height
+    createNew.style.height = '85vh'
+    
+    // change thw create text to 'x'
+    create.innerHTML = "&times;"
+    
+    // fade background elements
+    header.style.opacity = '.3'
+    main.style.opacity = '.3'
+    
+    // set opened to true
+    return opened = true
+    
+  }
+})
+
+
+
+
+
+
+
+const calcSoldLitre = (closing, opening) => {
+  let closingLitre = parseFloat(closing.replace(/,/g, ''));
+  let openingLitre = parseFloat(opening.replace(/,/g, ''));
+  let difference = closingLitre - openingLitre
+  
+  saleRecord.soldLitre.value = isNaN(difference) ? '--' : difference.toFixed(2)
+  return saleRecord.soldLitre.value = formatDigits(saleRecord.soldLitre.value);
+}
+
+const calcSoldAmount = (litre, price) => {
+  let soldLitre = parseFloat(litre.replace(/,/g, ''));
+  let sellingPrice = parseFloat(price.replace(/,/g, ''));
+  let product = soldLitre * sellingPrice 
+  
+  saleRecord.soldAmount.value = isNaN(product) ? '--' : product.toFixed(2)
+  return saleRecord.soldAmount.value = formatDigits(saleRecord.soldAmount.value);
+}
+
+const calcBalance = (amount, expenses, pos) => {
+  let saleAmount = parseFloat(amount.replace(/,/g, ''));
+  let saleExpenses = parseFloat(expenses.replace(/,/g, ''));
+  let salePos = parseFloat(pos.replace(/,/g, ''));
+  
+  let difference = saleAmount - saleExpenses - salePos 
+  
+  saleRecord.balance.value = isNaN(difference) ? '--' : difference.toFixed(2)
+  return saleRecord.balance.value = formatDigits(saleRecord.balance.value);
+}
+
+const calcShortage = (balance, bankcash, handcash) => {
+  let saleBalance = parseFloat(balance.replace(/,/g, ''));
+  let bankCash = parseFloat(bankcash.replace(/,/g, ''));
+  let handCash = parseFloat(handcash.replace(/,/g, ''));
+  
+  let difference = saleBalance - bankCash - handCash 
+  
+  saleRecord.shortage.value = isNaN(difference) ? '--' : difference.toFixed(2)
+  return saleRecord.shortage.value = formatDigits(saleRecord.shortage.value);
+}
+
+
+
+const saleRecord = {
+  sellingPeriod: document.querySelector('input[name=sellingPeriod]'),
+  sellingPrice: document.querySelector('input[name=sellingPrice]'),
+  closingLitre: document.querySelector('input[name=closingLitre]'),
+  openingLitre: document.querySelector('input[name=openingLitre]'),
+  soldLitre: document.querySelector('input[name=soldLitre]'),
+  soldAmount: document.querySelector('input[name=soldAmount]'),
+  handCash: document.querySelector('input[name=handCash]'),
+  bankCash: document.querySelector('input[name=bankCash]'),
+  expenses: document.querySelector('input[name=expenses]'),
+  pos: document.querySelector('input[name=pos]'),
+  balance: document.querySelector('input[name=balance]'),
+  shortage: document.querySelector('input[name=shortage]'),
+  saleValue: function (){
+    return ({
+      date: this.sellingPeriod.value,
+      price: this.sellingPrice.value,
+      closing: this.closingLitre.value,
+      opening: this.openingLitre.value,
+      litres: this.soldLitre.value,
+      amount: this.soldAmount.value,
+      cash_at_hand: this.handCash.value,
+      cash_to_bank: this.bankCash.value,
+      expenses: this.expenses.value,
+      pos: this.pos.value,
+      balance: this.balance.value,
+      shortage: this.shortage.value,
+    })
+  }
+}
+
+
+const sellingPeriod = saleRecord.sellingPeriod.value = new Date().toISOString().split('T')[0]
+const sellingPeriodDisp = document.querySelector('#sellingPeriodDisp')
+sellingPeriodDisp.innerHTML = new Date(sellingPeriod).toLocaleDateString('en-US', dateFormat)
+
+const submit = document.querySelector('#submit')
+
+saleRecord.sellingPeriod.onchange = e => {
+  sellingPeriodDisp.innerHTML = new Date(saleRecord.sellingPeriod.value).toLocaleDateString('en-US', dateFormat)
+}
+
+saleRecord.sellingPrice.onkeyup = e => {
+  saleRecord.sellingPrice.value = formatDigits(saleRecord.sellingPrice.value)
+  calcSoldAmount(saleRecord.soldLitre.value, saleRecord.sellingPrice.value)
+  calcBalance(saleRecord.soldAmount.value, saleRecord.expenses.value, saleRecord.pos.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+saleRecord.closingLitre.onkeyup = e => {
+  saleRecord.closingLitre.value = formatDigits(saleRecord.closingLitre.value)
+  calcSoldLitre(saleRecord.closingLitre.value, saleRecord.openingLitre.value)
+  calcSoldAmount(saleRecord.soldLitre.value, saleRecord.sellingPrice.value)
+  calcBalance(saleRecord.soldAmount.value, saleRecord.expenses.value, saleRecord.pos.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+saleRecord.openingLitre.onkeyup = e => {
+  saleRecord.openingLitre.value = formatDigits(saleRecord.openingLitre.value)
+  calcSoldLitre(saleRecord.closingLitre.value, saleRecord.openingLitre.value)
+  calcSoldAmount(saleRecord.soldLitre.value, saleRecord.sellingPrice.value)
+  calcBalance(saleRecord.soldAmount.value, saleRecord.expenses.value, saleRecord.pos.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+saleRecord.expenses.onkeyup = e => {
+  saleRecord.expenses.value = formatDigits(saleRecord.expenses.value)
+  calcBalance(saleRecord.soldAmount.value, saleRecord.expenses.value, saleRecord.pos.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+saleRecord.pos.onkeyup = e => {
+  saleRecord.pos.value = formatDigits(saleRecord.pos.value)
+  calcBalance(saleRecord.soldAmount.value, saleRecord.expenses.value, saleRecord.pos.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+saleRecord.bankCash.onkeyup = e => {
+  saleRecord.bankCash.value = formatDigits(saleRecord.bankCash.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+saleRecord.handCash.onkeyup = e => {
+  saleRecord.handCash.value = formatDigits(saleRecord.handCash.value)
+  calcShortage(saleRecord.balance.value, saleRecord.bankCash.value, saleRecord.handCash.value)
+}
+
+
+
+submit.onclick = e => {
+  // console.log(saleRecord.saleValue())
+  const {
+    date,
+    price,
+    closing,
+    opening,
+    litres,
+    amount,
+    expenses,
+    pos,
+    balance,
+    cash_at_hand,
+    cash_to_bank,
+    shortage
+  } = saleRecord.saleValue()
+  
+  insertDb({
+    date,
+    price,
+    closing,
+    opening,
+    litres,
+    amount,
+    expenses,
+    pos,
+    balance,
+    cash_at_hand,
+    cash_to_bank,
+    shortage
+  })
+}
---
+++ supabase.js
@@ -0,0 +1,214 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+// const { createClient } = supabase
+
+//const database_pass = "4nYwqkyZAziJX2jT";
+const supabaseUrl = "https://ujlsjsqhsizvvxhtmxff.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqbHNqc3Foc2l6dnZ4aHRteGZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1MDcyMTUsImV4cCI6MjA0MTA4MzIxNX0.pjW1X20P3Ag1oMXWKpUTWFQCQTHXjnxU4npBq3qpjnk"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const figurize = (figure) => {
+  return parseFloat(figure.replace(/,/g, ""))
+}
+
+const signUp = async (email, password) => {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+  })
+}
+const signIn = async (email, password) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+  email: 'example@email.com',
+  password: 'example-password',
+})
+
+}
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+}
+const resetPass = async (password) => {
+  const { data, error } = await supabase.auth.updateUser({
+    password: new_password
+  })
+}
+const recoverPass = async (email, password) => {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: 'https://example.com/update-password',
+  })
+}
+
+const userChange = () => {
+  supabase.auth.onAuthStateChange((event, session) => {
+    setTimeout(async () => {
+      // await on other Supabase function here
+      // this runs right after the callback has finished
+    }, 0)
+  })
+}
+
+
+// SELECT
+export const selectDb = async (prev, next) => {
+  if (next !== null) {
+    let { data: sales_transaction, error } = await supabase
+      .from('sales_transaction')
+      .select("*")
+      .gte('date', prev)
+      .lte('date', next)
+      
+      if (sales_transaction) {
+        // console.log(sales_transaction)
+        return sales_transaction
+      }
+      if (error) console.log(error)
+      // .gt('column', 'Greater than')
+      // .lt('column', 'Less than')
+
+  }
+  let { data: sales_transaction, error } = await supabase
+    .from('sales_transaction')
+    .select("*")
+    .eq('date', prev)
+    .single()
+    
+    if (sales_transaction) {
+      // console.log(sales_transaction)
+      return sales_transaction
+    }
+    if (error) {
+      console.log(error)
+      let data = {
+        price: '0.00',
+        closing: '0.00',
+        opening: '0.00',
+        litres: '0.00',
+        amount: '0.00',
+        expenses: '0.00',
+        pos: '0.00',
+        balance: '0.00',
+        cash_at_hand: '0.00',
+        cash_to_bank: '0.00',
+        shortage: '0.00',
+      }
+      
+      return data
+    }
+}
+
+// INSERT
+export const insertDb = async ({
+  date,
+  price,
+  closing,
+  opening,
+  litres,
+  amount,
+  expenses,
+  pos,
+  balance,
+  cash_at_hand,
+  cash_to_bank,
+  shortage
+  }) => {
+  try {
+    const { data, error } = await supabase
+      .from('sales_transaction')
+      .insert([
+        {
+          date: date,
+          price: figurize(price),
+          opening: figurize(opening),
+          closing: figurize(closing),
+          litres: figurize(litres),
+          amount: figurize(amount),
+          expenses: figurize(expenses),
+          pos: figurize(pos),
+          balance: figurize(balance),
+          cash_at_hand: figurize(cash_at_hand),
+          cash_to_bank: figurize(cash_to_bank),
+          shortage: figurize(shortage)
+           },
+        ])
+      .select()
+      
+      if (data) console.log('data added!')
+      if (error) console.log(error)
+  } catch (e) {
+    console.log(e)
+  }
+  
+}
+  
+// UPDATE
+const updateDb = async () => {
+  const { data, error } = await supabase
+    .from('sales_transaction')
+    .update({ other_column: 'otherValue' })
+    .eq('some_column', 'someValue')
+    .select()
+}
+  
+//DELETE
+const deleteDb = async () => {
+  const { error } = await supabase
+    .from('sales_transaction')
+    .delete()
+    .eq('some_column', 'someValue')
+}
+
+// SUBSCRIBE TO ALL CHANGES
+const channelsSub = () => {
+  const all_channels = supabase.channel('custom-all-channel')
+    .on(
+      'postgres_changes',
+      { event: '*', schema: 'public', table: 'sales_transaction' },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe()
+  
+  // SUBSCRIBE TO INSERT
+  const ins_channels = supabase.channel('custom-insert-channel')
+    .on(
+      'postgres_changes',
+      { event: 'INSERT', schema: 'public', table: 'sales_transaction' },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe()
+  
+  // SUBSCRIBE TO UPDATE 
+  const upd_channels = supabase.channel('custom-update-channel')
+    .on(
+      'postgres_changes',
+      { event: 'UPDATE', schema: 'public', table: 'sales_transaction' },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe()
+  
+  // SUBSCRIBE TO DELETE
+  const del_channels = supabase.channel('custom-delete-channel')
+    .on(
+      'postgres_changes',
+      { event: 'DELETE', schema: 'public', table: 'sales_transaction' },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe()
+  
+  // SUBSCRIBE TO SPECIFIC ROW CHANGES
+  const spf_channels = supabase.channel('custom-filter-channel')
+    .on(
+      'postgres_changes',
+      { event: '*', schema: 'public', table: 'sales_transaction', filter: 'some_column=eq.some_value' },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe()
+}
